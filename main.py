d6bf9544bdc1aace9dd5d3603ea54718b9a8d7b8
ffxxfan <443586186@qq.com>
2023-05-20 16:17:14 +0800

    提交

--- main.py
+++ main.py
@@ -3,7 +3,10 @@
 # @Time    : 2023/5/14 21:00
 # @Author  : Fengjie Fan
 # @File    : seam_carving.py
+import cv2
+
 import seam_carving
+import matplotlib.pyplot as plt
 
 
 # 按间距中的绿色按钮以运行脚本。
@@ -11,8 +14,8 @@ if __name__ == '__main__':
     # 图像无需标记保护区域或移除区域
     NO_MASK = ""
 
-    # 待处理图像路径
-    input_filename = ""
+    # 待处理图像路
+    input_filename = 'E:/lovePython/seam-carving-reproduction/images/in/madoka.jpg'
     # 图像处理完的输出路径
     output_filename = ""
     # 图像保护部分路径，如果没有则设置为 NO_MASK
@@ -26,5 +29,7 @@ if __name__ == '__main__':
     # 初始化
     my_seam_carving = seam_carving.SeamCarving(input_filename, output_filename, out_height, out_width, protected_mask, removal_mask)
     # 图像进行 seam-carving 处理并输出图像
-    my_seam_carving.image_resizing_without_mask()
-    my_seam_carving.save_image()
+    images = cv2.imread(input_filename)
+    plt.show(images)
+    # my_seam_carving.image_resizing_without_mask()
+    # my_seam_carving.save_image()
